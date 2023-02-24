32f423e3fc55d538b672256eccb8f820261f5764
Antonio <antoniodelaiglesia@gmail.com>
2023-02-24 14:06:39 +0100

    Feat: creating tables

--- src/models/characters.js
+++ src/models/characters.js
@@ -19,7 +19,7 @@ module.exports = (sequelize, DataTypes) => {
         defaultValue: DataTypes.UUIDV4,
         allowNull: false,
       },
-      charactersId: DataTypes.STRING,
+      characterId: DataTypes.STRING,
       name: DataTypes.STRING,
       species: DataTypes.STRING,
       house: DataTypes.STRING,
