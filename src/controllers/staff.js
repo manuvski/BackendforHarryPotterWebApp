abb294a8f05974357bee05206f0d34bb8051abe4
ManuelRod <manuvski@gmail.com>
2023-02-26 13:28:59 +0100

    feat:introduction of routes and controllers

---
+++ src/controllers/staff.js
@@ -0,0 +1,44 @@
+const db = require('../models')
+const Staff = db.Staff
+
+const getStaffList = async () => {
+    const staff = await Staff.findAll()
+    return staff
+}
+
+const getStaffById = async (id) => {
+    const staff = await Staff.findByPK(id)
+    return staff
+}
+
+const createStaff = async ({ nasaId, sol, image }) => {
+    const staff = await Staff.create({  nasaId, sol, image })
+    return staff
+}
+
+const updateStaff = async (id, data) => {
+    const staff = await Staff.update(data, {
+        where: {
+            id
+        }
+    })
+    return staff
+}
+
+const removeStaff = async (id) => {
+    await Staff.destroy({
+        where: {
+            id
+        }
+    })
+
+    return true
+}
+
+module.exports = {
+    getStaffList,
+    getStaffById,
+    createStaff,
+    updateStaff,
+    removeStaff
+}
