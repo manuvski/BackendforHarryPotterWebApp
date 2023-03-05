d993200da8029433d66b1ad5baf67e867f9cca5d
Antonio <antoniodelaiglesia@gmail.com>
2023-03-05 21:55:31 +0100

    Feat: Creation of intermediate table and implementation of favorites. Testing ok through insomnia

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,9 +1,9 @@
 const db = require('../models')
 const User = db.User
-const Characters = db.Characters
+const Character = db.Character
 
 const getUserById = async (id) => {
-  const user = await User.findByPK(id)
+  const user = await User.findOne(id)
   delete user.password
   return user
 }
@@ -11,31 +11,48 @@ const getUserByEmail = async (email) => {
   const user = await User.findOne({ where: { email: email } })
   return user
 }
-const toggleTaskToFavorite = async ({ userId, characterId }) => {
-  let user = await User.findByPk(userId, {
+const toggleCharacterToFav = async ({ userId, characterId }) => {
+  let user = await User.findOne({
+    where: { id: userId },
     attributes: { exclude: ['password', 'salt'] },
     include: {
-      model: db.Characters,
+      model: db.Character,
       as: 'favorites',
     },
   })
 
-  let currentFavList = user.favorites.map((item) => item.id) || []
+  // let character = await Character.findOne({
+  //   where: { id: characterId },
+  // })
+
+  // let currentFavList = user.favorites.map((item) => item.id) || []
+
+  console.log(user)
+  let currentFavList = (user.favorites || []).map((item) => item.id)
   console.log(currentFavList)
   const existed = currentFavList.includes(characterId)
-  let isAdded = false
+  let isAdded = true
 
   if (!existed) {
-    const characters = await Characters.findOne(characterId)
+    const characters = await Character.findOne({
+      where: { id: characterId },
+      include: {
+        model: db.User,
+        as: 'favoritedBy',
+      },
+    })
+
     if (!characters) {
       throw new Error('Character not found')
     }
     user.addFavorites(characters)
+    currentFavList.push(characterId)
     isAdded = true
     console.log(isAdded)
   } else {
     const newList = currentFavList.filter((item) => item !== characterId)
     user.setFavorites(newList)
+    isAdded = true
   }
   return { user, isAdded }
 }
@@ -57,7 +74,7 @@ const toggleTaskToFavorite = async ({ userId, characterId }) => {
 // }
 
 module.exports = {
-  toggleTaskToFavorite,
+  toggleCharacterToFav,
   getUserByEmail,
   getUserById,
 }
